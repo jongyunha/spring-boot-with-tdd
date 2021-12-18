13bcee62c8a38e280060e53e972e5a3ce0660694
nothingprogram <dev@jongyun.com>
2021-12-18 14:21:00 +0900

    Get the Data from Database tables with JPA find Methods using Controller

--- src/main/java/com/tdd/SpringBootRestService/SpringBootRestServiceApplication.java
+++ src/main/java/com/tdd/SpringBootRestService/SpringBootRestServiceApplication.java
@@ -1,13 +1,26 @@
 package com.tdd.SpringBootRestService;
 
+import com.tdd.SpringBootRestService.controller.Library;
+import com.tdd.SpringBootRestService.repository.LibraryRepository;
+import java.util.List;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringBootRestServiceApplication {
+public class SpringBootRestServiceApplication implements CommandLineRunner {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootRestServiceApplication.class, args);
-	}
+  @Autowired public LibraryRepository repository;
 
+  public static void main(String[] args) {
+    SpringApplication.run(SpringBootRestServiceApplication.class, args);
+  }
+
+  @Override
+  public void run(String[] args) {
+    Library lib = repository.findById("fdsefr343").get();
+    System.out.println(lib.getAuthor());
+  }
 }
--- src/main/java/com/tdd/SpringBootRestService/controller/Library.java
+++ src/main/java/com/tdd/SpringBootRestService/controller/Library.java
@@ -6,7 +6,6 @@ import javax.persistence.Id;
 import javax.persistence.Table;
 
 @Entity
-@Table(name = "LibraryDemo")
 public class Library {
   private String book_name;
 
