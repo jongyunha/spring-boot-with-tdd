4d12a672fde8ed1fe27595cf24bca647047a93a8
nothingprogram <dev@jongyun.com>
2021-12-19 12:01:45 +0900

    Response Entity object to get response and inject Junit Assertions to Test

---
+++ src/test/java/com/tdd/SpringBootRestService/controller/LibraryControllerTest.java
@@ -0,0 +1,44 @@
+package com.tdd.SpringBootRestService.controller;
+
+import static org.mockito.Mockito.when;
+
+import com.tdd.SpringBootRestService.repository.LibraryRepository;
+import com.tdd.SpringBootRestService.service.LibraryService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
+@SpringBootTest
+class LibraryControllerTest {
+
+  @Autowired LibraryController libraryController;
+  @MockBean LibraryRepository libraryRepository;
+  @MockBean LibraryService libraryService;
+
+  private Library library;
+
+  @BeforeEach
+  public void buildLibrary() {
+    library = new Library();
+    library.setAisle(322);
+    library.setBook_name("Spring");
+    library.setIsbn("sfe");
+    library.setAuthor("jongyunha");
+    library.setId("sfe322");
+  }
+
+  @Test
+  public void addBookTest() {
+    when(libraryService.buildId(library.getIsbn(), library.getAisle())).thenReturn(library.getId());
+    when(libraryService.checkBookAlreadyExist(library.getId())).thenReturn(false);
+
+    ResponseEntity<AddResponse> res = libraryController.addBookImplementation(library);
+    System.out.println(res.getBody());
+    Assertions.assertEquals(res.getStatusCode(), HttpStatus.CREATED);
+  }
+}
